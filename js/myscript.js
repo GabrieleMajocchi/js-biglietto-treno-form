4dcaac2700d25f4b8a9e3160db48504d969f273a
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-15 17:35:03 +0200

    added milestone 1 elements and scripts

---
+++ js/myscript.js
@@ -0,0 +1,20 @@
+
+const Sendit = document.querySelector('button')
+let TicketPrice;
+
+Sendit.addEventListener('click', 
+    function(){
+        let km = document.querySelector('#km')
+        let age = document.querySelector('#age')
+
+        if (age.value < 18){
+            TicketPrice = [(1000-194)/1000]*(km.value * 0.233)
+        } else if (age.value > 65){
+            TicketPrice = [(1000-377)/1000]*(km.value * 0.233)
+        } else {
+            TicketPrice = km.value * 0.21
+        }
+        TicketPrice = TicketPrice.toFixed(2);
+        console.log(TicketPrice+"€")
+    }
+)
