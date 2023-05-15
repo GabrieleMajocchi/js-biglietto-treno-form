03e31fbc045319bce59b06aacd1342c91ecaefa4
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-15 18:00:32 +0200

    changed things for milestone 2 so form, output on the page with the view of some data wroted by the user

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,7 @@
 
 const Sendit = document.querySelector('button')
 let TicketPrice;
+let Discount;
 
 Sendit.addEventListener('click', 
     function(){
@@ -9,11 +10,15 @@ Sendit.addEventListener('click',
 
         if (age.value < 18){
             TicketPrice = [(1000-194)/1000]*(km.value * 0.233)
+            Discount = 'grazie allo sconto del 19.4%' 
         } else if (age.value > 65){
             TicketPrice = [(1000-377)/1000]*(km.value * 0.233)
+            Discount = 'grazie allo sconto del 37.7%'
         } else {
             TicketPrice = km.value * 0.21
+            Discount = ''
         }
-        console.log(TicketPrice+"€")
+        TicketPrice = TicketPrice.toFixed(2);
+        document.getElementById("result").innerText ="Il costo del tuo biglietto per percorrere "+ km.value +"km è di "+ TicketPrice + "€ " + Discount;
     }
 )
