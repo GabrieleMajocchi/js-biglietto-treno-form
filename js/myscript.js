b22affc4b34d60bd792c7dd1a91b79ed68254bbe
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-16 11:08:49 +0200

    minor fix to the script structure

--- js/myscript.js
+++ js/myscript.js
@@ -6,19 +6,21 @@ let Discount;
 Sendit.addEventListener('click', 
     function(){
         let km = document.querySelector('#km')
+        km = parseInt(km.value)
         let age = document.querySelector('#age')
+        age = parseInt(age.value)
 
         if (age.value < 18){
-            TicketPrice = [(1000-194)/1000]*(km.value * 0.233)
+            TicketPrice = [(1000-194)/1000]*(km * 0.233)
             Discount = 'grazie allo sconto del 19.4%' 
         } else if (age.value > 65){
-            TicketPrice = [(1000-377)/1000]*(km.value * 0.233)
+            TicketPrice = [(1000-377)/1000]*(km * 0.233)
             Discount = 'grazie allo sconto del 37.7%'
         } else {
-            TicketPrice = km.value * 0.21
+            TicketPrice = km * 0.21
             Discount = ''
         }
         TicketPrice = TicketPrice.toFixed(2);
-        document.getElementById("result").innerText ="Il costo del tuo biglietto per percorrere "+ km.value +"km è di "+ TicketPrice + "€ " + Discount;
+        document.getElementById("result").innerText ="Il costo del tuo biglietto per percorrere "+ km +"km è di "+ TicketPrice + "€ " + Discount;
     }
 )
